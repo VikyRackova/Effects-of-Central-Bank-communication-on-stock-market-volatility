43111b0577915511e2168892c48166279741a47e
VikyRackova <viki.rackova@gmail.com>
2024-11-26 23:41:43 +0100

    Update Functions.R

--- Functions.R
+++ Functions.R
@@ -197,3 +197,221 @@ sentenceSplit <- function(text_df, extra_dividers = c(";", ":","--")) {
   sentence_df$nchar <- nchar(sentence_df$sentence)
   return(sentence_df)
 }
+
+
+### Function to process the Fourgrams
+process_fourgrams <- function(data, fourgrams_terms, four_grams_terms) {
+  data %>%
+    unnest_tokens(fourgram, Cleaned_Text, token = "ngrams", n = 4) %>%
+    group_by(Date, sentence_id) %>%
+    mutate(order = row_number()) %>%
+    # Check and replace fourgrams based on fourgrams_terms
+    mutate(fourgram = if_else(fourgram %in% fourgrams_terms$term, 
+                              str_replace_all(fourgram, " ", "_"), 
+                              fourgram)) %>%
+    arrange(Date, sentence_id, order) %>%
+    # Check if fourgram contains underscores and set skip logic
+    mutate(belongs_to_fourgrams = fourgram %in% four_grams_terms$term) %>%
+    # Determine merged_text based on row position and fourgram status
+    mutate(merged_text = case_when(
+      # Skip rows based on belongs_to_fourgrams logic
+      lag(belongs_to_fourgrams, default = FALSE) ~ "",    # Skip 1st row after underscore
+      lag(belongs_to_fourgrams, 2, default = FALSE) ~ "", # Skip 2nd row after underscore
+      lag(belongs_to_fourgrams, 3, default = FALSE) ~ "", # Skip 3rd row after underscore
+      # If it's the last row of the sentence_id group, take the whole fourgram
+      is.na(lead(sentence_id)) | lead(sentence_id) != sentence_id ~ fourgram,
+      # Otherwise, take the first word of the fourgram
+      TRUE ~ word(fourgram, 1)
+    )) %>%
+    # Keep rows with underscores and remove skipped rows
+    filter(merged_text != "" | belongs_to_fourgrams) %>%
+    # Concatenate the merged text into one sentence per Date and sentence_id
+    summarize(merged_sentence = str_c(merged_text, collapse = " "), .groups = 'drop')
+}
+
+
+### Function to process the Trigrams
+process_trigrams <- function(data, trigrams_terms, tri_grams_terms) {
+  data %>%
+    unnest_tokens(trigram, merged_sentence, token = "ngrams", n = 3) %>%
+    group_by(Date, sentence_id) %>%
+    mutate(order = row_number()) %>%
+    # Check and replace trigrams based on trigrams_terms
+    mutate(trigram = if_else(trigram %in% trigrams_terms$term, 
+                             str_replace_all(trigram, " ", "_"), 
+                             trigram)) %>%
+    arrange(Date, sentence_id, order) %>%
+    # Check if trigram contains underscores and set skip logic
+    mutate(belongs_to_trigrams = trigram %in% tri_grams_terms$term) %>%
+    # Determine merged_text based on row position and trigram status
+    mutate(merged_text = case_when(
+      lag(belongs_to_trigrams, default = FALSE) ~ "",    # Skip 1st row after underscore
+      lag(belongs_to_trigrams, 2, default = FALSE) ~ "", # Skip 2nd row after underscore
+      # If it's the last row of the sentence_id group, take the whole trigram
+      is.na(lead(sentence_id)) | lead(sentence_id) != sentence_id ~ trigram,
+      # Otherwise, take the first word of the trigram
+      TRUE ~ word(trigram, 1)
+    )) %>%
+    # Keep rows with underscores and remove skipped rows
+    filter(merged_text != "" | belongs_to_trigrams) %>%
+    # Concatenate the merged text into one sentence per Date and sentence_id
+    summarize(merged_sentence = str_c(merged_text, collapse = " "), .groups = 'drop')
+}
+
+
+### Function to process the Bigrams
+process_bigrams <- function(data, bigrams_terms, bi_grams_terms) {
+  data %>%
+    unnest_tokens(bigram, merged_sentence, token = "ngrams", n = 2) %>%
+    group_by(Date, sentence_id) %>%
+    mutate(order = row_number()) %>%
+    # Check and replace bigrams based on bigrams_terms
+    mutate(bigram = if_else(bigram %in% bigrams_terms$term, 
+                            str_replace_all(bigram, " ", "_"), 
+                            bigram)) %>%
+    arrange(Date, sentence_id, order) %>%
+    # Check if bigram contains underscores and set skip logic
+    mutate(belongs_to_bigrams = bigram %in% bi_grams_terms$term) %>%
+    # Determine merged_text based on row position and bigram status
+    mutate(merged_text = case_when(
+      lag(belongs_to_bigrams, default = FALSE) ~ "",    # Skip 1st row after underscore
+      # If it's the last row of the sentence_id group, take the whole bigram
+      is.na(lead(sentence_id)) | lead(sentence_id) != sentence_id ~ bigram,
+      # Otherwise, take the first word of the bigram
+      TRUE ~ word(bigram, 1)
+    )) %>%
+    # Keep rows with underscores and remove skipped rows
+    filter(merged_text != "" | belongs_to_bigrams) %>%
+    # Concatenate the merged text into one sentence per Date and sentence_id
+    summarize(merged_sentence = str_c(merged_text, collapse = " "), .groups = 'drop')
+}
+
+
+### Function that assigns value to word combinations found in the dictionary
+calculate_scores <- function(Clean_file, All_terms) {
+  # Step 1: Process and clean tokens
+  Term <- Clean_file %>%
+    unnest_tokens(word, merged_sentence, token = "ngrams", n = 1) %>% # create individual words
+    group_by(Date, sentence_id) %>%
+    mutate(order = row_number()) %>%
+    mutate(word = if_else(word %in% All_terms$term, word, NA_character_)) %>%
+    filter(!is.na(word)) %>%  # Remove rows with NA in the word column (words not in All_terms)
+    left_join(All_terms %>% dplyr::select(term, category, topic), by = c("word" = "term"), relationship = "many-to-many")
+
+  # Step 2: Identify keywords and modifiers
+  keywords <- Term %>%
+    filter(category %in% c("Hawkish Keyword", "Dovish Keyword"))
+
+  modifiers <- Term %>%
+    filter(category %in% c("High modifier", "Low modifier", "Positive modifier", "Negative modifier"))
+  
+  negators <- Term %>%
+    filter(category %in% "Negators")
+  
+  # Step 3: Join keywords and modifiers, calculate distances
+  Score_with_distances <- modifiers %>%
+    left_join(keywords, by = c("Date", "sentence_id","word_count"), suffix = c("_modifier", "_keyword"), relationship = "many-to-many") %>%
+    mutate(distance = abs(order_modifier - order_keyword)) %>% # define distances between keywords and modifiers
+    group_by(Date, sentence_id, word_modifier) %>%
+    filter(distance == min(distance) | (distance == min(distance) & order_keyword < order_modifier)) %>% # pair modifiers with keywords that are closest to it
+    ungroup() %>%
+    dplyr::select(-topic_modifier)  # Remove the topic_modifier column
+  
+  # Step 4: Assign scores based on modifier type and associated keyword category (based on rules below)
+  Score <- Score_with_distances %>%
+    group_by(Date, sentence_id) %>%
+    mutate(
+      has_keyword = any(category_keyword %in% c("Hawkish Keyword", "Dovish Keyword")),
+      negator_in_front = lag(word_modifier) %in% negators & lag(order_modifier) == order_modifier - 1) %>%
+    ungroup() %>%
+    mutate(score = case_when(
+      
+      !has_keyword & category_modifier == "High modifier" ~ 1,
+      !has_keyword & category_modifier == "Positive modifier" ~ 1,
+      !has_keyword & category_modifier == "Low modifier" ~ -1,
+      !has_keyword & category_modifier == "Negative modifier" ~ -1,
+      
+      category_modifier == "High modifier" & category_keyword == "Hawkish Keyword" ~ 1,
+      category_modifier == "High modifier" & category_keyword == "Dovish Keyword" ~ -1,
+      category_modifier == "Low modifier" & category_keyword == "Hawkish Keyword" ~ -1,
+      category_modifier == "Low modifier" & category_keyword == "Dovish Keyword" ~ 1,
+      
+      category_modifier == "Positive modifier" & category_keyword == "Hawkish Keyword" & !topic_keyword %in% c("Prices", "Policy") ~ 1,
+      category_modifier == "Positive modifier" & category_keyword == "Dovish Keyword" & !topic_keyword %in% c("Prices", "Policy") ~ -1,
+      category_modifier == "Positive modifier" & category_keyword == "Dovish Keyword" & topic_keyword == "Prices" ~ 1,
+      category_modifier == "Positive modifier" & topic_keyword == "Policy" ~ -1,
+      category_modifier == "Positive modifier" & category_keyword == "Hawkish Keyword" & topic_keyword == "Prices" ~ -1,
+      
+      category_modifier == "Negative modifier" & category_keyword == "Hawkish Keyword" & !topic_keyword %in% c("Prices", "Policy") ~ -1,
+      category_modifier == "Negative modifier" & category_keyword == "Dovish Keyword" & !topic_keyword %in% c("Prices", "Policy") ~ 1,
+      category_modifier == "Negative modifier" & category_keyword == "Dovish Keyword" & topic_keyword == "Prices" ~ -1,
+      category_modifier == "Negative modifier" & category_keyword == "Hawkish Keyword" & topic_keyword == "Prices" ~ 1,
+      category_modifier == "Negative modifier" & topic_keyword == "Policy" ~ 1,
+      TRUE ~ 0  # Default case if no conditions match
+    )) %>%
+    
+    # Step 6: Reverse score if a negator is found directly before the modifier
+    mutate(score = if_else(negator_in_front, -score, score)) %>%
+    
+  # Return the final scored data frame
+  return(Score)
+}
+
+
+### Function to calculate a standardized score per document and per topic
+standardized_score_and_topic <- function(data){
+  sentence<- data%>%
+    group_by(Date,sentence_id,topic_keyword)%>%
+    summarise(word_count = first(word_count),  # Retain word_count by taking the first value in each group
+              score =(sum(score, na.rm = TRUE))/word_count, # create an average score per sentence per topic
+              .groups = 'drop')%>%
+    group_by(Date,sentence_id)%>%
+    mutate(sentence_score = sum(score))%>% # create a score per sentence
+    ungroup()
+
+  # generate a number of sentences per document
+  sentence_count <- data %>%
+    group_by(Date) %>%
+    summarise(
+      num_sentences = n_distinct(sentence_id),  # Count unique sentence IDs
+      .groups = 'drop'
+    )
+  
+  # create a fraction each topic represents per sentence
+  topic_fraction<- sentence%>% 
+    left_join(sentence_count, by = "Date")%>%
+    group_by(Date,topic_keyword)%>%
+    mutate(
+      topic_fraction_sentence= ifelse(
+        sentence_score == 0 ,             #  denominator is 0
+        score,                             # Keep the numerator
+        score / sentence_score))%>%            # Perform the division otherwise
+    mutate(topic_fraction = sum(topic_fraction_sentence)/num_sentences)%>%
+    dplyr::select(Date,topic_keyword,topic_fraction)
+    
+  topic_fraction <- unique(topic_fraction)
+  
+  # Deduplicate sentence_scores
+  unique_sentence_scores <- sentence  %>%
+    distinct(Date, sentence_id, .keep_all = TRUE)  # Keep unique sentence IDs for each Date
+  
+  # calculate a score per document and standardize it
+  total_score <- unique_sentence_scores%>%
+    left_join(sentence_count, by = "Date")%>%
+    group_by(Date) %>%
+    summarise(total_document_score = round(sum((sentence_score), na.rm = TRUE) / first(num_sentences), 3),  # Use num_sentences in the calculation
+              .groups = 'drop'
+    )%>%
+    mutate(Standardized_score = ((total_document_score - mean(total_document_score))/sd(total_document_score)) )
+
+  # summarise the results and create standardized scores per topic
+  standardized_topic_scores <- topic_fraction%>%
+    left_join(total_score, by = "Date")%>%
+    mutate(standardized_topic_score = Standardized_score * topic_fraction)%>%
+    dplyr::select(Date,topic_keyword,standardized_topic_score,Standardized_score)%>%
+    tidyr::pivot_wider(
+      names_from = topic_keyword, 
+      values_from = standardized_topic_score,
+      values_fill = 0 ) # Fill missing values with 0
+  return(standardized_topic_scores)
+}
